7177cfc4832dd06551883d654af6444c0843f485
Nabil-Siddique <77914380+Nabil-Siddique@users.noreply.github.com>
2021-04-17 21:20:11 +0600

    Add files via upload

---
+++ Database_connection_demo/Database_connection_demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Database_connection_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection = @"Server=DESKTOP-LSE8ACET\SQL_SERVER;Database=OOP2_H_SP21;User Id=ka;Password=PONG;";
+            SqlConnection conn = new SqlConnection(connection);
+            conn.Open();
+
+        }
+    }
+}
